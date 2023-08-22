0dd644c8604642b8cdad070e2ff876cd0c5814b6
darinetag <tagdarinefatma@gmail.com>
2023-08-22 22:05:09 +0100

    to do list, done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+    const form = document.getElementById('create-task-form');
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      addTaskToList();
+    });
+    function addTaskToList() {
+      const inputField = document.getElementById('new-task-description');
+      const taskDescription = inputField.value;
+    
+      if (taskDescription.trim() === '') {
+        return; 
+      }
+    
+      const taskList = document.getElementById('tasks');
+      const newItem = document.createElement('li');
+      newItem.textContent = taskDescription;
+      taskList.appendChild(newItem);
+    
+      inputField.value = '';
+    }
+    
+  });
+  
+ 
+
